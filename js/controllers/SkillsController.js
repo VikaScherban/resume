5854afa054783f0fae17e976de8ce9c333abe3ab
Victoriya <www.viki.wwww@rambler.ru>
2018-09-07 16:17:50 +0300

    added skills

--- js/controllers/SkillsController.js
+++ js/controllers/SkillsController.js
@@ -2,7 +2,8 @@ resumeApp.controller('SkillsCtrl', function ($scope, $http, $location) {
     var url = "resources/skills.json";
 
     $http.get(url).success(function (data) {
-        $scope.skills = data;
+        $scope.allSkills = data;
+        $scope.skills = $scope.allSkills;
     }).error(function (e) {
         alert("Error of get data.\n" + e);
     });
